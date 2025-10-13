009ea660b9299fdd77a3f00cade718c8c9582762
Dougal Matthews <dougal@dougalmatthews.com>
2025-10-13 12:52:31 +0000

    feat: add Homebrew tap for glug
    - Add glug.rb formula with v1.0.1 release
    - Include proper SHA256 hash for security
    - Add comprehensive README with usage examples
    - Enable brew install d0ugal/glug/glug

--- homebrew-glug/glug.rb
+++ homebrew-glug/glug.rb
@@ -1,8 +1,8 @@
 class Glug < Formula
   desc "JSON log parser and colorizer"
   homepage "https://github.com/d0ugal/glug"
-  url "https://github.com/d0ugal/glug/archive/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  url "https://github.com/d0ugal/glug/archive/v1.0.1.tar.gz"
+  sha256 "b417f5bc803df6b81d3ef5574f637d6cddc4d73f2a690bd2c88f613ec9fbaa59"
   license "MIT"
 
   depends_on "go" => :build
